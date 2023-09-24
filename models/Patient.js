4b5bc02a948d358e475f42585fd966bd1e3b687f
shakkya-j <145644454+shakkya-j@users.noreply.github.com>
2023-09-25 02:23:50 +0530

    Updating Patient.js in models in backend

--- models/Patient.js
+++ models/Patient.js
@@ -1,1 +1,87 @@
-i
+const mongoose = require("mongoose");  //Import mongoose which interact with MongoDB
+const schema = mongoose.Schema; //Assigns mongoose.Schema constructor
+
+const patientSchema = new schema ({      //Defines the patient schema
+  
+  email: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  nic: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: false,
+  },
+  phoneNo: {
+    type: Number,
+    required: true,
+  },
+  civilStatus: {
+    type: String,
+    required: true,
+  },
+  height: {
+    type: Number,
+    required: true,
+  },
+  weight: {
+    type: Number,
+    required: true,
+  },
+  bloodGroup: {
+    type: String,
+    required: true,
+  },
+  medicalStatus: {
+    type: String,
+    required: true,
+  },
+  allergies: {
+    type: String,
+    required: true,
+  },
+  emergencyPhone: {
+    type: Number,
+    required: true,
+  },
+  gaurdianName: {
+    type: String,
+    required: false,
+  },
+  gaurdianPhone: {
+    type: Number,
+    required: false,
+  },
+  gaurdianNIC: {
+    type: String,
+    required: false,
+  },
+  insuranceNo: {
+    type: String,
+    required: false,
+  },
+  insuranceCompany: {
+    type: String,
+    required: false,
+  },
+  
+});
