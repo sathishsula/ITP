2cf6b236856ba0161536466e7742f8046999662c
Sathish Sulakshitha <sathishped860@gmail.com>
2023-09-25 02:36:42 +0530

    read and  data up to date in database

--- routes/order.js
+++ routes/order.js
@@ -26,3 +26,29 @@ router.route("/add").post((req,res)=>{
         console.log(error);
     })
 });
+
+//data read
+
+router.route("/").get((req,res)=>{
+    OrderI.find().then((user)=>{
+        res.json(user);
+    }).catch((error)=>{
+        console.log(error)
+    })
+});
+
+//data update
+router.route("/update/:id").put(async(req,res)=>{
+    let userID = req.params.id;
+    const {order_id,supplier,date,destination,quantity} = req.body;
+
+    const updateUser = {
+        order_id,supplier,date,destination,quantity
+    }
+    const update = await OrderI.findByIdAndUpdate(userID,updateUser).then(()=>{
+        res.status(200).send({status:"Inventory update"});
+    }).catch((error)=>{
+        console.log(error);
+        res.status(500).send({status:"error updating data!", error:error.message});
+    });
+});
