e61e7742833c14e63303c7d231cd464cf064cb39
IT21241000 <110513655+IT21241000@users.noreply.github.com>
2023-10-13 13:32:31 +0530

    Update appointment.js
    creating a new appointment                                                                       create (sending mail)

--- routes/appointment.js
+++ routes/appointment.js
@@ -36,3 +36,115 @@ router.route("/patientAppointments/:id").get(async (req, res) => {
       });
     });
 });
+
+
+// creating a new appointment 
+router.route("/makeapt").post(async (req, res) => {
+  const patient = req.body.patient;
+  const notes = req.body.notes;
+  const channel = req.body.channel;
+  const name = req.body.name;
+  const address = req.body.address;
+  const age = req.body.age;
+  const gender = req.body.gender;
+  const contact = req.body.contact;
+  
+//sending mail 
+  const transporter = nodemailer.createTransport({
+    host: "smtp.zoho.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "hospitalitp@zohomail.com",
+      pass: "Sliit@321",
+    },
+  });
+
+  console.log(channel);
+  const cid = channel._id;
+  const doctor = channel.doctor;
+  const startDateTime = channel.startDateTime;
+  const maxPatients = channel.maxPatients;
+  const drName = channel.drName;
+  const completed = channel.completed;
+  var patients = channel.patients;
+
+  console.log(drName);
+
+  patients = parseInt(patients);
+  patients++;
+
+  const appointmentNo = patients;
+
+  let arrivalTime = new Date(startDateTime);
+  arrivalTime.setMinutes(
+    arrivalTime.getMinutes() + 15 * (parseInt(appointmentNo) - 1)
+  );
+  console.log(arrivalTime.toLocaleString());
+
+  const newAppointment = new Appointment({
+    channel,
+    patient,
+    // name,
+    appointmentNo,
+    notes,
+    arrivalTime,
+    name,
+    address,
+    age,
+    gender,
+    contact,
+  });
+
+  const updateChannel = {
+    doctor,
+    drName,
+    startDateTime,
+    maxPatients,
+    patients,
+    completed,
+  };
+
+  newAppointment
+    .save()
+    .then(() => {
+      const update = Channel.findByIdAndUpdate(cid, updateChannel)
+        .then(() => {
+          const usr = Patient.findById(patient)
+            .then((pt) => {
+              const mailOptions = {
+                from: "hospitalitp@zohomail.com",
+                to: `${pt.email}`,
+                subject: "Appointment Made",
+                text: `Hello \nYour Appointment has been made for Dr.${drName}.\n Appointment No :${appointmentNo} 
+                    \nDate Time ${new Date(
+                      startDateTime
+                    ).toString()} \n\n\ Be there at approximately  ${arrivalTime.toLocaleString()} to avoid waiting.`,
+              };
+
+              transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                  console.log(error);
+                } else {
+                  console.log("Email sent: " + info.response);
+                }
+              });
+
+              res.json("New appointment Added ");
+            })
+            .catch((err) => {
+              console.log(err.message);
+              res.status(500).send({
+                status: "Error in getting patient details",
+                error: err.message,
+              });
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
